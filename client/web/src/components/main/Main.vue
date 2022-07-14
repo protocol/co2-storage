6c3b14df9242d0f47075fda0d12ef191a63c8e4b
adgsm <momcilo@dzunic.net>
2022-07-14 20:27:47 +0200

    Front page skeleton done

--- client/web/src/components/main/Main.vue
+++ client/web/src/components/main/Main.vue
@@ -9,6 +9,7 @@
 				<div class="header-bar-rest">
 					<div class="header-bar-rest-item">{{ $t("message.main.header.about") }}</div>
 					<div class="header-bar-rest-item">{{ $t("message.main.header.stats") }}</div>
+					<div class="header-bar-rest-item highlighted">{{ $t("message.main.header.connect-wallet") }}</div>
 				</div>
 			</div>
 		</div>
@@ -17,6 +18,26 @@
 			<div class="body-sub-heading">
 				<span class="body-sub-heading-sentence">{{ $t("message.main.body.sub-title") }}</span>
 				<span class="body-sub-heading-sentence colored left-margin-1">{{ $t("message.main.body.sub-title-colored") }}</span>
+			</div>
+			<div class="body-item-columns">
+				<div class="body-item-column title">{{ $t("message.main.body.stats") }}</div>
+				<div class="body-item-column link">{{ $t("message.main.body.view-all-stats") }}</div>
+			</div>
+			<div class="body-item">
+				<div class="body-item-title">{{ $t("message.main.body.cumulative-uploads") }}</div>
+				<div class="body-item-description">{{ $t("message.main.body.cumulative-uploads-description") }}</div>
+			</div>
+			<div class="body-item">
+				<div class="body-item-title">{{ $t("message.main.body.average-uploads-per-month") }}</div>
+				<div class="body-item-description">{{ $t("message.main.body.average-uploads-per-month-description") }}</div>
+			</div>
+			<div class="body-item">
+				<div class="body-item-title">{{ $t("message.main.body.cumulative-cids") }}</div>
+				<div class="body-item-description">{{ $t("message.main.body.cumulative-cids-description") }}</div>
+			</div>
+			<div class="body-item">
+				<div class="body-item-title">{{ $t("message.main.body.cumulative-environmental-asset-templates") }}</div>
+				<div class="body-item-description">{{ $t("message.main.body.cumulative-environmental-asset-templates-description") }}</div>
 			</div>
 		</div>
 	</section>
--- client/web/src/locales/en_GB.js
+++ client/web/src/locales/en_GB.js
@@ -3,12 +3,23 @@ export default {
 		"main": {
 			"header": {
 				"about": "about",
-				"stats": "stats"
+				"stats": "stats",
+				"connect-wallet": "connect wallet"
 			},
 			"body": {
 				"title": "CO2.Storage",
 				"sub-title": "Free decentralized storage for Environmental Assets",
-				'sub-title-colored': "on IPFS and Filecoin"
+				"sub-title-colored": "on IPFS and Filecoin",
+				"stats": "stats",
+				"view-all-stats": "view all stats",
+				"cumulative-uploads": "Cumulative uploads",
+				"cumulative-uploads-description": "Total amount of environmental assets stored on the dweb",
+				"average-uploads-per-month": "Average uploads per month",
+				"average-uploads-per-month-description": "Average monthly uploads",
+				"cumulative-cids": "Cumulative CIDs",
+				"cumulative-cids-description": "Total amount of CIDs created from environmental assets uploads",
+				"cumulative-environmental-asset-templates": "Cumulative Environmental Asset Templates",
+				"cumulative-environmental-asset-templates-description": "Total amount of templates created from environmental assets uploads"
 			}
 		}
 	}
