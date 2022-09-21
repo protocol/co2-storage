14187e50c9824c231ac6b3ba19f907fca65823ec
Marc Johnson <44822571+mjohnson518@users.noreply.github.com>
2022-09-21 06:42:30 -0600

    Update Main.vue

--- client/landing-page/src/components/main/Main.vue
+++ client/landing-page/src/components/main/Main.vue
@@ -11,13 +11,13 @@
 				<div class="body-item-title">CO2.storage</div>
 				<div class="body-item-description">{{ $t("message.about.body.about-description") }}</div>
 				<div class="body-item-description">
-					<span class="body-item-about-description">{{ $t("message.about.body.about-description-1") }} <a class="body-item-link" href="https://github.com/protocol/FilecoinGreen-tools/blob/main/0006-FGTP-CO2_Storage.md"><u>0006-FGTP-CO2_Storage</u></a></span>
+					<span class="body-item-about-description">{{ $t("message.about.body.about-description-1") }} <a class="body-item-link" href="https://github.com/protocol/FilecoinGreen-tools/blob/main/0006-FGTP-CO2_Storage.md" target="_blank"><u>0006-FGTP-CO2_Storage</u></a></span>
 				</div>
 				<div class="body-item-description">
-					<span class="body-item-about-description">{{ $t("message.about.body.about-description-6") }} <a class="body-item-link" href="https://forms.gle/CS5tY4kpsajpiTGV7"><u>CO2.Storage Updates Form</u></a></span>	
+					<span class="body-item-about-description">{{ $t("message.about.body.about-description-6") }} <a class="body-item-link" href="https://forms.gle/CS5tY4kpsajpiTGV7" target="_blank"><u>CO2.Storage Updates Form</u></a></span>	
 				</div>
 				<div class="body-item-description">
-					<span class="body-item-about-description">{{ $t("message.about.body.about-description-7") }} <a class="body-item-link" href="https://green.filecoin.io"><u>Filecoin Green</u></a> {{ $t("message.about.body.about-description-8") }}.</span>		
+					<span class="body-item-about-description">{{ $t("message.about.body.about-description-7") }} <a class="body-item-link" href="https://green.filecoin.io" target="_blank"><u>Filecoin Green</u></a> {{ $t("message.about.body.about-description-8") }}.</span>		
 				</div>
 			</div>
 		</div>
