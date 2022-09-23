48354d0b26f7561ac40bc176e8b5e380aadf9efe
Marc_Johnson <marcjohnson518@gmail.com>
2022-09-23 09:28:59 +0200

    clean up

--- client/landing-page/README 2.md
+++
@@ -1,9 +0,0 @@
-# co2.storage landing page
-
-### Use
-
-### ToDo
-
-### License
-Licensed under the MIT license.
-http://www.opensource.org/licenses/mit-license.php
--- client/web/src/components/about/About.vue
+++ client/web/src/components/about/About.vue
@@ -14,18 +14,18 @@
 				<span class="body-item-about-description">{{ $t("message.about.body.about-description") }}</span>	
 			</div>
 			<div class="body-item-columns">
-				<span class="body-item-about-description">{{ $t("message.about.body.about-description-1") }} <a class="body-item-link" href="https://github.com/protocol/FilecoinGreen-tools/blob/main/0006-FGTP-CO2_Storage.md"><u>0006-FGTP-CO2_Storage</u></a></span></div>
+				<span class="body-item-about-description">{{ $t("message.about.body.about-description-1") }} <a class="body-item-link" href="https://github.com/protocol/FilecoinGreen-tools/blob/main/0006-FGTP-CO2_Storage.md" target="_blank"><u>0006-FGTP-CO2_Storage</u></a></span></div>
 			<div class="body-item-columns">
-				<span class="body-item-about-description">{{ $t("message.about.body.about-description-2") }} <a class="body-item-link" href="https://ipld.io/"><u>(IPLD DAGs)</u></a> {{ $t("message.about.body.about-description-3") }}</span>	
+				<span class="body-item-about-description">{{ $t("message.about.body.about-description-2") }} <a class="body-item-link" href="https://ipld.io/" target="_blank"><u>(IPLD DAGs)</u></a> {{ $t("message.about.body.about-description-3") }}</span>	
 			</div>
 			<div class="body-item-columns">
-				<span class="body-item-about-description">{{ $t("message.about.body.about-description-4") }} <a class="body-item-link" href="https://docs.ipfs.io/concepts/content-addressing/"><u>content identifier</u></a> {{ $t("message.about.body.about-description-5") }}</span>	
+				<span class="body-item-about-description">{{ $t("message.about.body.about-description-4") }} <a class="body-item-link" href="https://docs.ipfs.io/concepts/content-addressing/" target="_blank"><u>content identifier</u></a> {{ $t("message.about.body.about-description-5") }}</span>	
 			</div>
 			<div class="body-item-columns">
-				<span class="body-item-about-description">{{ $t("message.about.body.about-description-6") }} <a class="body-item-link" href="mailto: green@filecoin.org"><u>green@filecoin.org</u></a></span>	
+				<span class="body-item-about-description">{{ $t("message.about.body.about-description-6") }} <a class="body-item-link" href="https://forms.gle/CS5tY4kpsajpiTGV7" target="_blank"><u>CO2.Storage Updates Form</u></a></span>	
 			</div>
 			<div class="body-item-columns">
-				<span class="body-item-about-description">{{ $t("message.about.body.about-description-7") }} <a class="body-item-link" href="https://green.filecoin.io"><u>Filecoin Green</u></a> {{ $t("message.about.body.about-description-8") }}.</span>		
+				<span class="body-item-about-description">{{ $t("message.about.body.about-description-7") }} <a class="body-item-link" href="https://green.filecoin.io" target="_blank"><u>Filecoin Green</u></a> {{ $t("message.about.body.about-description-8") }}.</span>		
 			</div>
 		</div>
 	</section>
