f1210a65f7aad012de04b7c85f9fe6f16c25f9ac
adgsm <momcilo@dzunic.net>
2023-03-27 17:23:42 +0400

    corrected fgApiUrl in existing examples

--- cli/src/examples/get_account.js
+++ cli/src/examples/get_account.js
@@ -5,7 +5,7 @@ const ipfsNodeType = "client"
 //const ipfsNodeAddr = "/ip4/127.0.0.1/tcp/5001"
 //const fgApiUrl = "http://localhost:3020"
 const ipfsNodeAddr = "/dns4/web2.co2.storage/tcp/5002/https"
-const fgApiUrl = "https://co2.storage"
+const fgApiUrl = "https://web2.co2.storage"
 
 const fgStorage = new FGStorage({authType: authType, ipfsNodeType: ipfsNodeType, ipfsNodeAddr: ipfsNodeAddr, fgApiHost: fgApiUrl})
 
